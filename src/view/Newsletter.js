afbf2c7340bb3eb500165bcdfd3d29471ca43980
Matt Day <mattday@matts-mac-mini.lan>
2023-09-18 17:50:56 -0400

    9/18 commit

--- src/view/Newsletter.js
+++ src/view/Newsletter.js
@@ -6,7 +6,7 @@ export default function Newsletter() {
     return (
         <div className='fullpage'>
             <div className='row newsletter d-flex justify-content-center'>
-                <iframe loading="lazy" src="https://www.canva.com/design/DAFuDAJTyf4/xci1EGZxFm7ieMqhLAWucA/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe>
+                <iframe loading="lazy" src="https://www.canva.com/design/DAFuidJ4mVE/Pdkt5cdUZf2G4Fo4OXYCbQ/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">  </iframe>
             </div>
             <div className='calendar-load'>
                 <CircularProgress color='inherit' size={60} />
