8070f2d5b5ca473b379b46f118461699667185e2
Matt Day <mattday@matts-mac-mini.lan>
2023-12-15 17:31:24 -0500

    added handbook

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -39,13 +39,13 @@ export default function Nav({random}) {
               </div>
             </a>
             <a className="nav-item dropdown nav-dropdown-menu">
-              {/* <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Documents
               </a>
               <div className="dropdown-menu nav-dropdown-menu">
                 <a className='dropdown-item' target='_blank' href='https://docs.google.com/document/d/e/2PACX-1vQPeJBqkW7yVvrsCaLjGqYF2fyNDIPCRIlaF6WHy1csc5xtrt4xUVkKCrkIGdQG8Q/pub'>ByLaws</a>
-              </div> */}
-              <a className='nav-link' target='_blank' href='https://docs.google.com/document/d/e/2PACX-1vQPeJBqkW7yVvrsCaLjGqYF2fyNDIPCRIlaF6WHy1csc5xtrt4xUVkKCrkIGdQG8Q/pub'>ByLaws</a>
+                <a className='dropdown-item' target='_blank' href='https://docs.google.com/document/d/10wykCkms8F9Mv4ZVhM3Wy-Do_dlNp0w1_EhSQ7snDbs/edit?usp=sharing'>Student Handbook</a>
+              </div>
             </a>
             <a className="nav-item">
               <Link className="nav-link" to={'/support'}>Support Us</Link>
