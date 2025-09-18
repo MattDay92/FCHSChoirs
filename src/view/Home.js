2d8dd34c28ffee0c6be086733f4c12a7c43e6b29
Matt Day <mattday@matts-mac-mini.lan>
2025-09-18 18:08:52 -0400

    9/18 commit

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import popupIMG from '../components/photos/FreddiesFundraiser.webp'
+import popupIMG from '../components/photos/JacksDonutsDTD.webp'
 import Showfest from '../components/photos/ShowFestLogo.webp'
 import HeritageSingers from '../components/photos/Heritage2025Comp.webp'
 import SigPic from '../components/photos/SigFall.webp'
@@ -8,7 +8,7 @@ import SensationsPic from '../components/photos/Sensations2025Comp.webp'
 import LegacyPic from '../components/photos/CombinedGirlsFall.webp'
 import BellaPic from '../components/photos/BellaFall.webp'
 import Button from '@mui/material/Button';
-import Sponsor1 from '../components/photos/Generations.webp'
+import Sponsor1 from '../components/photos/Farley.webp'
 import Sponsor2 from '../components/photos/Paul-Humbles.webp'
 import Sponsor3 from '../components/photos/tri-kappa-logo.png'
 import Featured1 from '../components/photos/Featured1.jpg'
@@ -22,16 +22,16 @@ export default function Home({ background, closeAd, showPopUp, featuredInfo }) {
   return (
     <div className='fullpage'>
 
-      {/* {showPopUp === true ? <>
+      {showPopUp === true ? <>
         <div id='popup'>
           <div className='row concert-ad'>
             <button className='popup-close-btn' onClick={closeAd}><i class="fa-solid fa-x"></i></button>
             <div className='col-4 popup-img'>
-              <img className='popup-logo' alt='Spring Concert Poster' src={popupIMG} />
+              <img className='popup-logo' alt='Dine to Donate' src={popupIMG} />
             </div>
             <div className='col-4 popup-info text-center'>
               <h3 className='text-white'>Dine to Donate</h3>
-              <p>Support the Franklin Choirs by dining at Freddie's in Franklin on August 21st!  Don't forget to show the flyer!
+              <p>Support the Franklin Choirs by dining at Jack's Donuts in Franklin on Thursday, 9/25!  Don't forget to tell them to support the FCHS Choirs!
               </p>
               
             </div>
@@ -40,7 +40,7 @@ export default function Home({ background, closeAd, showPopUp, featuredInfo }) {
       </>
       :
       <></>
-      } */}
+      }
 
       {background === 'sensations' ? <>
         <div className='hero-image'>
@@ -123,57 +123,53 @@ export default function Home({ background, closeAd, showPopUp, featuredInfo }) {
         <div className='sponsors1'>
           <h3>Director's Circle - $1,000 +</h3>
           <div className='d-flex justify-content-around flex-wrap'>
-            {/* <div className='sponsor-img-div sponsor1-img-div'><img className='sponsor-img' alt='Generations of Graham' src={Sponsor1} /></div> */}
+            <div className='sponsor-img-div sponsor1-img-div'><img className='sponsor-img' alt='Generations of Graham' src={Sponsor1} /></div>
             <div className='sponsor-img-div sponsor1-img-div'><img className='sponsor-img' alt='Paul Humbles Agency' src={Sponsor2} /></div>
-            {/* <div className='sponsor-img-div sponsor1-img-div'><img className='sponsor-img w-50' alt='Zeta Chapter of Tri Kappa Inc.' src={Sponsor3} /></div> */}
           </div>
-          {/* <div>
-            <h4>Duane & Vonda Mercer</h4>
-          </div> */}
+          <div className='directors-circle-text'>
+            <h4>Jason & Melissa Sailor</h4>
+            <h4>Michael & Denita Pickett</h4>
+          </div>
         </div>
         <div className='sponsors2'>
           <div>
             <h3>Platinum Level - $500 - $999</h3>
-            {/* <div className='d-flex justify-content-around flex-wrap'>
+            <div className='d-flex justify-content-around flex-wrap'>
               <div className='sponsor-col'>
                 <h5>Mayor Steve Barnett</h5>
                 <h5>JWG Inc.</h5>
-                <h5>Commercial Sewer Cleaning Company Inc.</h5>
-                <h5>Farley Health Solutions</h5>
-                <h5>Greene's Auto & Tire Service</h5>
-                <h5>Michael & Denita Pickett</h5>
+                <h5>Commercial Sewer Cleaning</h5>
               </div>
               <div className='sponsor-col'>
                 <h5>Grandpa Jeff's Trail Rides</h5>
-                <h5>Remix Pest Control</h5>
                 <h5>Franklin, Muffler, Brakes & More</h5>
-                <h5>Beck Automotive</h5>
-                <h5>Pennington Brothers Enterprise</h5>
-                <h5>Tom Thompson</h5>
               </div>
-            </div> */}
+            </div>
           </div>
           <div className='sponsor-col bottom-sponsor-2'>
             <h3>Gold Level - $250 - $499</h3>
-            {/* <h5>Bradley Chevrolet</h5>
-            <h5>Under One Woof</h5>
-            <h5>The Dave Price Team Mortgage Lender</h5>
-            <h5>Promise Advisory Group</h5> */}
+            <h5>Bradley Hubler Chevrolet</h5>
+            <h5>Baldwin Excavating</h5>
+            <h5>Vaught Family Eye Care</h5>
           </div>
         </div>
         <div className='sponsors3 d-flex justify-content-center'>
           <div className='sponsor-col'>
             <h3>Silver Level</h3>
-            {/* <h5>Baird Wealth Management</h5>
-            <h5>Franklin Insurance Agency</h5>
-            <h5>PrintWorks</h5>
-            <h5>T-Shirt Express</h5>
-            <h5>Ray & Carmen Lane</h5>
-            <h5>Nimble Thimbles Quilt Club</h5> */}
+            <h5>Spotlight Scoopers</h5>
+            <h5>Irish Brothers Pest Control</h5>
+            
           </div>
           <div className='sponsor-col'>
             <h3>Bronze Level</h3>
-            
+            <h5>A Trophy Business</h5>
+            <h5>PrintWorks</h5>
+            <h5>Johnson County REMC</h5>
+            <h5>Noble Roman's</h5>
+            <h5>Millie's Ice Cream</h5>
+            <h5>Ritters Frozen Custard</h5>
+            <h5>M&M Custard (Freddie's)</h5>
+            <h5>Jill Fleener</h5>
           </div>
 
         </div>
